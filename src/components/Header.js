a2274d673cdfd6d6ba46844c3896b160f3330b5f
David Vargas <davidvargas95@gmail.com>
2021-03-27 23:34:38 -0700

    Added header and body to the Page.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="jumbotron bg-info">
+            <div className="text-center text-white">
+                <h1 className="display-2 font-weight-bold">User Directory </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Page.js
@@ -0,0 +1,16 @@
+import React from "react";
+import API from "../utils/API";
+import Header from "./Header";
+
+
+function Test() {
+    return(
+        <div>
+            <Header />
+
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Test;
